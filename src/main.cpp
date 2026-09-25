041e35c2f7ffcf13753cb95bde681d8554ff3f25
Lakshya894-hue <sharmalakshya593@gmail.com>
2026-09-25 22:37:16 +0530

    feat: integrate Passenger Management into main menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 #include "FlightManager.h"
+#include "PassengerManager.h"
 
 using namespace std;
 
-int main()
+void flightMenu(FlightManager& flightManager)
 {
-    FlightManager manager;
-
     int choice;
 
     do
     {
-        cout << "\n\n========== AEROSTRUCT ==========\n";
+        cout << "\n========== Flight Management ==========\n";
         cout << "1. Add Flight\n";
         cout << "2. Display Flights\n";
         cout << "3. Search Flight\n";
@@ -19,50 +18,139 @@ int main()
         cout << "5. Delete Flight\n";
         cout << "6. Cancel Flight\n";
         cout << "7. Sort Flights\n";
-        cout << "8. Exit\n";
-
-        cout << "\nEnter your choice: ";
+        cout << "0. Back\n";
+        cout << "Enter your choice: ";
         cin >> choice;
 
-        switch(choice)
+        switch (choice)
         {
             case 1:
-                manager.addFlight();
+                flightManager.addFlight();
                 break;
 
             case 2:
-                manager.displayFlights();
+                flightManager.displayFlights();
                 break;
 
             case 3:
-                manager.searchFlight();
+                flightManager.searchFlight();
                 break;
 
             case 4:
-                manager.updateFlight();
+                flightManager.updateFlight();
                 break;
 
             case 5:
-                manager.deleteFlight();
+                flightManager.deleteFlight();
                 break;
 
             case 6:
-                manager.cancelFlight();
+                flightManager.cancelFlight();
                 break;
 
             case 7:
-                manager.sortFlights();
+                flightManager.sortFlights();
                 break;
 
-            case 8:
-                cout << "Exiting AeroStruct...\n";
+            case 0:
+                cout << "Returning to main menu...\n";
                 break;
 
             default:
-                cout << "Invalid choice.\n";
+                cout << "Invalid choice. Try again.\n";
         }
 
-    } while (choice != 8);
+    } while (choice != 0);
+}
+
+
+void passengerMenu(PassengerManager& passengerManager)
+{
+    int choice;
+
+    do
+    {
+        cout << "\n========== Passenger Management ==========\n";
+        cout << "1. Add Passenger\n";
+        cout << "2. Display Passengers\n";
+        cout << "3. Search Passenger\n";
+        cout << "4. Update Passenger\n";
+        cout << "5. Delete Passenger\n";
+        cout << "0. Back\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+            case 1:
+                passengerManager.addPassenger();
+                break;
+
+            case 2:
+                passengerManager.displayPassengers();
+                break;
+
+            case 3:
+                passengerManager.searchPassenger();
+                break;
+
+            case 4:
+                passengerManager.updatePassenger();
+                break;
+
+            case 5:
+                passengerManager.deletePassenger();
+                break;
+
+            case 0:
+                cout << "Returning to main menu...\n";
+                break;
+
+            default:
+                cout << "Invalid choice. Try again.\n";
+        }
+
+    } while (choice != 0);
+}
+
+
+int main()
+{
+    FlightManager flightManager;
+    PassengerManager passengerManager;
+
+    int choice;
+
+    do
+    {
+        cout << "\n========================================\n";
+        cout << "        AEROSTRUCT AIRPORT SYSTEM       \n";
+        cout << "========================================\n";
+        cout << "1. Flight Management\n";
+        cout << "2. Passenger Management\n";
+        cout << "0. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+            case 1:
+                flightMenu(flightManager);
+                break;
+
+            case 2:
+                passengerMenu(passengerManager);
+                break;
+
+            case 0:
+                cout << "Thank you for using AeroStruct!\n";
+                break;
+
+            default:
+                cout << "Invalid choice. Try again.\n";
+        }
+
+    } while (choice != 0);
 
     return 0;
 }
